73b846ef65921d2f0b94966d794f442311a39922
attt <ttt3pu@gmail.com>
2021-06-30 12:41:22 +0900

    やっぱりvh調整無いと変なので戻す

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,6 +28,24 @@ export default defineComponent({
   setup() {
     const _year = computed(() => new Date().getFullYear());
 
+    if (process.client) {
+      const setFillHeight = () => {
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+      }
+
+      let beforeHeight = window.innerHeight;
+
+      window.addEventListener('resize', () => {
+        if (beforeHeight !== window.innerHeight) {
+          beforeHeight = window.innerHeight;
+          setFillHeight();
+        }
+      });
+
+      setFillHeight();
+    }
+
     return {
       _year,
     };
