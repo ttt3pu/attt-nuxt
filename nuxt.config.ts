bc168a2ed5eca693c99f000d4ccc2f9cef00556b
attt <ttt3pu@gmail.com>
2022-09-11 02:22:25 +0900

    chore: rm unused plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,10 +33,6 @@ export default defineNuxtConfig({
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    {
-      src: '@/plugins/vue-clickaway',
-      ssr: false,
-    },
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
--- plugins/vue-clickaway.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import { directive as onClickaway } from 'vue-clickaway';
-
-Vue.directive('onClickaway', onClickaway);
