b6cedd3a162e8b943d6f279759b31109e1afacc0
attt <ttt3pu@gmail.com>
2023-10-07 21:11:24 +0900

    feat: Display posts from other sites

---
+++ constants/otherPosts.ts
@@ -0,0 +1,10 @@
+import { MergedPost } from '@/types';
+
+export const otherPosts: MergedPost[] = [
+  {
+    type: 'other',
+    title: '【Nuxt 3移行】ユニットテストをNuxt 2から移行し、実行速度が4倍速くなった話',
+    date: '2023-09-27',
+    link: 'https://tech.medpeer.co.jp/entry/2023/09/27/094500',
+  },
+];
