e90ddb7db5d08239601b0acb4f7f8e680f028ba5
attt <ttt3pu@gmail.com>
2024-02-17 02:43:59 +0900

    upgrade: @sidebase/nuxt-auth to latest

--- server/middleware/adminCheck.ts
+++ server/middleware/adminCheck.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
     // FIXME: type errorになる
     //   型 'import("/home/attt/Projects/attt-nuxt/node_modules/.pnpm/h3@1.10.1/node_modules/h3/dist/index").H3Event<import("/home/attt/Projects/attt-nuxt/node_modules/.pnpm/h3@1.10.1/node_modules/h3/dist/index").EventHandlerRequest>' の引数を型 'import("/home/attt/Projects/attt-nuxt/node_modules/.pnpm/h3@1.8.2/node_modules/h3/dist/index").H3Event<import("/home/attt/Projects/attt-nuxt/node_modules/.pnpm/h3@1.8.2/node_modules/h3/dist/index").EventHandlerRequest>' のパラメーターに割り当てることはできません。
     // 'context.matchedRoute' の型は、これらの型同士で互換性がありません。ts(2345)
-    const session = await getServerSession(event as any);
+    const session = await getServerSession(event);
     const isAdmin = session?.user?.email === 'ttt3pu@gmail.com';
 
     if (!isAdmin) {
