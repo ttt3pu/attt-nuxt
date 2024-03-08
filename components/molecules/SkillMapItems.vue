c5336cccd15099c3046aa0c4702dfbc433c4e45d
attt <ttt3pu@gmail.com>
2024-03-09 01:20:51 +0900

    feat: Renew skillmap

---
+++ components/molecules/SkillMapItems.vue
@@ -0,0 +1,72 @@
+<template>
+  <ul class="mb-4 font-en flex flex-wrap" :size="size">
+    <li v-for="(item, i) in items" :key="i" class="text-white mb-3 flex items-baseline">
+      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" :style="`fill: #${item.icnData.hex};`">
+        <path :d="item.icnData.path" />
+      </svg>
+
+      <span>{{ item.heading }}</span>
+      <span v-if="i !== items.length - 1" class="mr-3 ml-7 text-gray-500">/</span>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+import type { SimpleIcon } from 'simple-icons';
+
+defineProps({
+  items: {
+    required: true,
+    type: Array as PropType<
+      {
+        heading: String;
+        icnData: SimpleIcon;
+      }[]
+    >,
+  },
+  size: {
+    required: true,
+    type: Number as PropType<1 | 2 | 3>,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+li {
+  &:not(:last-child) {
+    @apply mr-4;
+  }
+
+  span {
+    @apply relative;
+    @apply bottom-[5.5px];
+  }
+}
+
+[size='1'] {
+  li {
+    @apply text-2xl;
+  }
+
+  svg {
+    @apply w-9;
+    @apply h-9;
+    @apply mr-3;
+    @apply relative;
+    @apply top-[3px];
+  }
+}
+
+[size='2'],
+[size='3'] {
+  li {
+    @apply text-xl;
+  }
+
+  svg {
+    @apply w-7;
+    @apply h-7;
+    @apply mr-3;
+  }
+}
+</style>
---
+++ components/organisms/SkillMap.vue
@@ -0,0 +1,211 @@
+<template>
+  <div>
+    <v-heading-lv2 class="font-en"> Skills </v-heading-lv2>
+
+    <v-heading-lv3 class="font-en">Primary</v-heading-lv3>
+    <p class="font-jp text-white mb-6">メイン業務で使用中</p>
+    <MoleculesSkillMapItems :items="primaryItems" :size="1" />
+
+    <v-heading-lv3 class="font-en">Secondary</v-heading-lv3>
+    <p class="font-jp text-white mb-6">サブ業務で使用中</p>
+    <MoleculesSkillMapItems :items="secondaryItems" :size="2" />
+
+    <v-heading-lv3>Tertiary</v-heading-lv3>
+    <p class="font-jp text-white mb-6">勉強中 or 古いバージョンなら使ったことがある</p>
+    <MoleculesSkillMapItems :items="tertiaryItems" :size="3" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {
+  siCss3,
+  siGrunt,
+  siHtml5,
+  siJavascript,
+  siJest,
+  siJquery,
+  siNuxtdotjs,
+  siPug,
+  siReact,
+  siSass,
+  siStorybook,
+  siTailwindcss,
+  siTypescript,
+  siVuedotjs,
+  siWebpack,
+  siRubyonrails,
+  siSwagger,
+  siGithubactions,
+  siCircleci,
+  siVite,
+  siGraphql,
+  siVitest,
+  siNodedotjs,
+  siHasura,
+  siTerraform,
+  siPostgresql,
+  siAmazonaws,
+  siMysql,
+  siPrisma,
+  siNetlify,
+  siVercel,
+  siAwsamplify,
+  siAwslambda,
+  siNextdotjs,
+  siRenovatebot,
+  siDocker,
+} from 'simple-icons';
+
+const primaryItems = [
+  {
+    heading: 'Vue',
+    icnData: siVuedotjs,
+  },
+  {
+    heading: 'Nuxt',
+    icnData: siNuxtdotjs,
+  },
+  {
+    heading: 'TypeScript',
+    icnData: siTypescript,
+  },
+  {
+    heading: 'Vitest',
+    icnData: siVitest,
+  },
+  {
+    heading: 'Tailwind',
+    icnData: siTailwindcss,
+  },
+  {
+    heading: 'Jest',
+    icnData: siJest,
+  },
+  {
+    heading: 'JavaScript',
+    icnData: siJavascript,
+  },
+  {
+    heading: 'HTML',
+    icnData: siHtml5,
+  },
+  {
+    heading: 'CSS',
+    icnData: siCss3,
+  },
+];
+
+const secondaryItems = [
+  {
+    heading: 'Rails',
+    icnData: siRubyonrails,
+  },
+  {
+    heading: 'Node.js',
+    icnData: siNodedotjs,
+  },
+  {
+    heading: 'GH Actions',
+    icnData: siGithubactions,
+  },
+  {
+    heading: 'Hasura',
+    icnData: siHasura,
+  },
+  {
+    heading: 'GraphQL',
+    icnData: siGraphql,
+  },
+  {
+    heading: 'Terraform',
+    icnData: siTerraform,
+  },
+  {
+    heading: 'AWS',
+    icnData: siAmazonaws,
+  },
+  {
+    heading: 'Amplify',
+    icnData: siAwsamplify,
+  },
+  {
+    heading: 'Lambda',
+    icnData: siAwslambda,
+  },
+  {
+    heading: 'postgresql',
+    icnData: siPostgresql,
+  },
+  {
+    heading: 'Swagger',
+    icnData: siSwagger,
+  },
+  {
+    heading: 'Renovate',
+    icnData: siRenovatebot,
+  },
+  {
+    heading: 'Docker',
+    icnData: siDocker,
+  },
+  {
+    heading: 'Circle CI',
+    icnData: siCircleci,
+  },
+  {
+    heading: 'Scss',
+    icnData: siSass,
+  },
+  {
+    heading: 'Vite',
+    icnData: siVite,
+  },
+];
+
+const tertiaryItems = [
+  {
+    heading: 'React',
+    icnData: siReact,
+  },
+  {
+    heading: 'Next.js',
+    icnData: siNextdotjs,
+  },
+  {
+    heading: 'Prisma',
+    icnData: siPrisma,
+  },
+  {
+    heading: 'Netlify',
+    icnData: siNetlify,
+  },
+  {
+    heading: 'Vercel',
+    icnData: siVercel,
+  },
+  {
+    heading: 'webpack',
+    icnData: siWebpack,
+  },
+  {
+    heading: 'Stoybook',
+    icnData: siStorybook,
+  },
+  {
+    heading: 'jQuery',
+    icnData: siJquery,
+  },
+  {
+    heading: 'MySQL',
+    icnData: siMysql,
+  },
+  {
+    heading: 'Grunt',
+    icnData: siGrunt,
+  },
+  {
+    heading: 'Pug',
+    icnData: siPug,
+  },
+];
+</script>
