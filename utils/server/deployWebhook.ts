a169632b3ec6733510ff4297adddc0a462723716
attt <ttt3pu@gmail.com>
2025-01-12 02:02:40 +0900

    fix: deploy webhook fetch

--- utils/server/deployWebhook.ts
+++ utils/server/deployWebhook.ts
@@ -2,7 +2,7 @@ export async function deployWebhook() {
   const { DEPLOY_WEBHOOK_URL } = useRuntimeConfig();
 
   if (DEPLOY_WEBHOOK_URL) {
-    await useFetch(DEPLOY_WEBHOOK_URL, {
+    await fetch(DEPLOY_WEBHOOK_URL, {
       method: 'POST',
     });
   }
