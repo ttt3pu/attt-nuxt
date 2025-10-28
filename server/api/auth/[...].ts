33dd60cbf39f62f661712b18160e698aaf48f8ac
attt <ttt3pu@gmail.com>
2025-10-28 14:34:33 +0000

    chore(deps): add next-auth version 4.21.1 to package.json and pnpm-lock.yaml

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -3,10 +3,10 @@ import GoogleProvider from 'next-auth/providers/google';
 
 export default NuxtAuthHandler({
   providers: [
-    // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
+    // @ts-expect-error - You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GoogleProvider.default({
-      clientId: process.env.AUTH_CLIENT_ID_GOOGLE,
-      clientSecret: process.env.AUTH_CLIENT_SECRET_GOOGLE,
+      clientId: process.env.AUTH_CLIENT_ID_GOOGLE!,
+      clientSecret: process.env.AUTH_CLIENT_SECRET_GOOGLE!,
     }),
   ],
   secret: process.env.AUTH_SECRET,
